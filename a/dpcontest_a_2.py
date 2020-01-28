78270b82fbcace8a64076dfe5c94766a47082468
A.Arimura <404ari@gmail.com>
2020-01-28 23:06:52 +0900

    add dp contest a

--- a/dpcontest_a_2.py
+++ a/dpcontest_a_2.py
@@ -4,9 +4,8 @@ n = int(sys.stdin.readline().strip('\n'))
 h = list(map(int, sys.stdin.readline().strip('\n').split()))
 dp = [float('inf') for _ in range(n)]
 dp[0] = 0
-dp[1] = abs(h[0]-h[1])
-for i in range(2, n):
-  # 集めるdp
-  dp[i] = min(dp[i-1] + abs(h[i-1]-h[i]), 
-             dp[i-2] + abs(h[i-2]-h[i]))
+for i in range(n):
+  # 配るdp
+  if i+1<=n-1: dp[i+1] = min(dp[i+1], dp[i] + abs(h[i]-h[i+1]))
+  if i+2<=n-1: dp[i+2] = min(dp[i+2], dp[i] + abs(h[i]-h[i+2])) 
 print(dp[n-1])
