cfcb3bbb2712a1575c7779ec5a5c8ffdb6db57bb
A.Arimura <404ari@gmail.com>
2020-05-19 00:50:42 +0900

    add abc168 a, b, and c

--- c/abc168c.py
+++ c/abc168c.py
@@ -7,4 +7,12 @@ deg = abs(a_deg - b_deg) if abs(a_deg - b_deg) <= 180 else 360 - abs(a_deg - b_d
 # print(f'{deg=}')
 dis_sqrt = a**2 + b**2 - 2 * a * b * math.cos(math.radians(deg))
 ans = math.sqrt(dis_sqrt)
-print(ans)
+# print(ans)
+
+# 極座標を計算して２点のeuclid距離
+a_th = math.radians(a_deg)
+b_th = math.radians(b_deg)
+a_x, a_y = a * math.cos(a_th), a * math.sin(a_th)
+b_x, b_y = b * math.cos(b_th), b * math.sin(b_th)
+dis = math.sqrt((b_x - a_x) ** 2 + (b_y - a_y) ** 2)
+print(dis)
