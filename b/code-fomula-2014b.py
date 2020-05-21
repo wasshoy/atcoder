3acc3af26d9120e53b5738ce435366f5231e6cd2
A.Arimura <404ari@gmail.com>
2020-05-21 22:02:04 +0900

    [solved] バチャコン3
    まいにちバチャコン3回目

---
+++ b/code-fomula-2014b.py
@@ -0,0 +1,22 @@
+import sys
+
+
+def LI(): return list(map(int, sys.stdin.buffer.readline().split()))
+def I(): return int(sys.stdin.buffer.readline())
+def LS(): return sys.stdin.buffer.readline().rstrip().decode('utf-8').split()
+def S(): return sys.stdin.buffer.readline().rstrip().decode('utf-8')
+def IR(n): return [I() for i in range(n)]
+def LIR(n): return [LI() for i in range(n)]
+def SR(n): return [S() for i in range(n)]
+def LSR(n): return [LS() for i in range(n)]
+def SRL(n): return [list(S()) for i in range(n)]
+def MSRL(n): return [[int(j) for j in list(S())] for i in range(n)]
+
+
+n = S()
+ans1 = sum([int(i) for i in n[0::2]])
+ans2 = sum([int(i) for i in n[1::2]])
+if len(n) % 2 == 0:
+    print(ans1, ans2)
+else:
+    print(ans2, ans1)
