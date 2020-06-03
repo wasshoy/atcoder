46ed1441de567f0d1984fe2c6942267ba64f373a
A.Arimura <404ari@gmail.com>
2020-06-03 16:05:38 +0900

    [update] 習作追加
    素数判定、試し割りに依る素因数分解を行うスクリプトを追加

---
+++ others/prime-factrizations/judge_prime.py
@@ -0,0 +1,13 @@
+# 素数判定 O(√N)
+import math
+
+
+def is_prime(n):
+    if n == 1:
+        return False
+    if n % 2 == 0:
+        return False
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
---
+++ others/prime-factrizations/prime_fact.py
@@ -0,0 +1,37 @@
+# 素因数分解 O(√N)
+# 試し割り法
+# nより小さい数で順に割っていき割り切れるかどうかで素因数の判定を行う
+# 素因数が見つかった場合、その素因数で割り切れなくなるまでnを更新する
+# Nの素数は最大でも√NであることよりO(√N)で計算可能
+# (素因数とその個数はcollections のCounterを使うと手軽)
+
+from collections import Counter
+
+
+def trial_divide(n):
+    if n < 2:
+        return
+    p_factors = []
+    # 2だけ先に取り分けておく
+    while n % 2 == 0:
+        p_factors.append(2)
+        n //= 2
+    # 残りの奇数の素因数を探す
+    f = 3
+    while f * f <= n:
+        if n % f == 0:
+            p_factors.append(f)
+            n //= f
+        else:
+            f += 2
+    # nが素数の場合
+    if n != 1:
+        p_factors.append(n)
+    return p_factors
+
+
+# 素因数とその個数のタプルのリストを返す
+def get_prime_counts(n):
+    p_factors = trial_divide(n)
+    counter = Counter(p_factors)
+    return list(counter.items())
