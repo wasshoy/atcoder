49ea9230d8d95792eab35ef84577ba536a9f51fc
A.Arimura <404ari@gmail.com>
2020-06-24 03:39:16 +0900

    [update] 解答追加

---
+++ c/abc016c.py
@@ -0,0 +1,45 @@
+from collections import deque
+import sys
+
+
+def input(): return sys.stdin.readline().strip()
+def I(): return int(input())
+def LI(): return list(map(int, input().split()))
+def IR(n): return [I() for i in range(n)]
+def LIR(n): return [LI() for i in range(n)]
+
+
+INF = float('inf')
+
+
+def bfs(g, start):
+    '''startからの最短距離を返す'''
+    n = len(g)
+    d = [INF] * n
+    que = deque([start])
+    d[start] = 0
+    while que:
+        now = que.popleft()
+        for next in g[now]:
+            if d[next] != INF:
+                continue
+            que.append(next)
+            d[next] = d[now] + 1
+    return d
+
+
+def main():
+    n, m = LI()
+    ab = LIR(m)
+    edges = [[] for _ in range(n)]
+    for a, b in ab:
+        a, b = a - 1, b - 1
+        edges[a].append(b)
+        edges[b].append(a)
+    # 各ユーザーからの最短距離が 2 のユーザーの数を出力
+    for i in range(n):
+        d = bfs(edges, i)
+        print(d.count(2))
+
+
+main()
