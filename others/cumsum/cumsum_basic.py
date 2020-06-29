dcf164fc1b55b5f536a9a8ae51a967c9db933243
A.Arimura <404ari@gmail.com>
2020-06-29 11:06:38 +0900

    [update] ツール修正

--- others/cumsum/cumsum_basic.py
+++ others/cumsum/cumsum_basic.py
@@ -5,4 +5,9 @@ def calc_cumsum(l):
     return cumsum
 
 
-print(calc_cumsum([1, 1, 2, 3, 5, 8, 13, 21, 34]))
+if __name__ == '__main__':
+    l = [1, 1, 2, 3, 5, 8, 13, 21, 34]
+    print(f'元の配列 : {l}')
+    ruiseki = calc_cumsum(l)
+    print(f'累積和 :   {ruiseki}')
+    print(f'l[4]から l[7]の和 (区間 [4, 8) の和) : {ruiseki[8] - ruiseki[4]=}')
