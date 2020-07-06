dc9b20dafd8529be5ad26e24c6ae13bf3b97d45b
A.Arimura <404ari@gmail.com>
2020-07-06 13:05:17 +0900

    [update] スニペット修正

---
+++ snippets/run-length-encoding/rle.py
@@ -0,0 +1,17 @@
+def calc_rle_l(s):
+    '''要素が [文字, 文字数] の リストをかえす'''
+    now_c = s[0]
+    cnt = 1
+    res = []
+    for i in range(1, len(s)):
+        if now_c == s[i]:
+            cnt += 1
+        else:
+            res.append([now_c, cnt])
+            now_c = s[i]
+            cnt = 1
+    res.append([now_c, cnt])
+    return res
+
+
+print(calc_rle_l('BBBWW'))
