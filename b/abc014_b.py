79cff928f24669768872cafcbf4fd86191847e9b
A.Arimura <404ari@gmail.com>
2020-07-20 22:18:31 +0900

    💪🏻 解答追加

---
+++ b/abc014_b.py
@@ -0,0 +1,26 @@
+# 6m AC
+import sys
+
+
+def input(): return sys.stdin.readline().strip()
+def I(): return int(input())
+def LI(): return list(map(int, input().split()))
+def IR(n): return [I() for i in range(n)]
+def LIR(n): return [LI() for i in range(n)]
+def SR(n): return [S() for i in range(n)]
+def S(): return input()
+def LS(): return input().split()
+
+
+INF = float('inf')
+
+
+n, x = LI()
+a = LI()
+x = bin(x)
+x = x[2:][::-1]
+ans = 0
+for i, keta in enumerate(x):
+    if keta == '1':
+        ans += a[i]
+print(ans)
