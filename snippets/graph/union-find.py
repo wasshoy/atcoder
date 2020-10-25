7f5faa4080bfeaa5c28c1ef4f5e5070b9faafb20
A.Arimura <404ari@gmail.com>
2020-10-25 20:54:17 +0900

    snippent修正

--- snippets/graph/union-find.py
+++ snippets/graph/union-find.py
@@ -1,70 +1,42 @@
-from collections import defaultdict
-from copy import deepcopy
+from collections import Counter
 import sys
 
 
 def input(): return sys.stdin.readline().strip()
-def I(): return int(input())
 def LI(): return list(map(int, input().split()))
 
 
-class UnionFind():
-    def __init__(self, n):
-        self.n = n
-        # 各要素の親要素 親は -(その集合の要素数)
-        self.parents = [-1] * n
-
-    def find(self, x):
-        '''要素 x の親要素の番号を返す'''
-        if self.parents[x] < 0:
-            return x
-        else:
-            self.parents[x] = self.find(self.parents[x])
-            return self.parents[x]
-
-    def union(self, x, y):
-        '''要素 x が属する集合と要素 y が属する集合を併合する'''
-        x = self.find(x)
-        y = self.find(y)
-
-        if x == y:
-            return
-
-        if self.parents[x] > self.parents[y]:
-            x, y = y, x
-
-        self.parents[x] += self.parents[y]
-        self.parents[y] = x
-
-    def size(self, x):
-        '''要素 x が属する集合の要素数を返す'''
-        return -self.parents[self.find(x)]
-
-    def same(self, x, y):
-        '''要素 x と要素 y が同じ集合に属するかを返す'''
-        return self.find(x) == self.find(y)
-
-    def members(self, x):
-        '''要素 x が属する集合の要素をリストを返す'''
-        root = self.find(x)
-        return [i for i in range(self.n) if self.find(i) == root]
-
-    def roots(self):
-        return [i for i, x in enumerate(self.parents) if x < 0]
-
-    def group_count(self):
-        return len(self.roots())
-
-    def all_group_members(self):
-        return {r: self.members(r) for r in self.roots()}
-
-    def __str__(self):
-        return '\n'.join('{}: {}'.format(r, self.members(r)) for r in self.roots())
+n, m = LI()
+parents = [i for i in range(n)]
 
 
-def main():
-    n, m = LI()
-    edges = [LI() for _ in range(m)]
+def root(x):
+    if parents[x] == x:
+        return x
+    else:
+        return root(parents[x])
 
 
-main()
+def unite(x, y):
+    root_x = root(x)
+    root_y = root(y)
+    if root_x == root_y:
+        return
+    parents[root_x] = parents[root_y]
+
+
+def same(x, y):
+    return root(x) == root(y)
+
+
+for _ in range(m):
+    a, b = LI()
+    a -= 1
+    b -= 1
+    unite(a, b)
+
+
+print(parents)
+c = Counter(parents)
+ans = max(c.values())
+print(ans)
