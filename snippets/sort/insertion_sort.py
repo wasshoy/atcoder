9e4becadf8b59d340f06b9de2fc500c3ec5ebed9
A.Arimura <404ari@gmail.com>
2020-11-27 14:18:23 +0900

    ⚙️  スニペット修正
    挿入ソート
    試しやすいように追記

--- snippets/sort/insertion_sort.py
+++ snippets/sort/insertion_sort.py
@@ -1,16 +1,23 @@
+# 挿入ソート
 # 先頭の要素からはじめて、 1 ~ i 番目 要素の中でa[i]を適切な場所へ移動する
 # O(n ^ 2)
 def insert_sort(a):
     n = len(a)
     for i in range(1, n):
         v = a[i]  # 挿入する要素
+        print(f'挿入する要素：{a[i]=}')
         for j in reversed(range(i)):  # 後ろから見ていく
+            print(f'{a[j]=} と {v=}')
             if a[j] > v:
                 a[j], a[j + 1] = a[j + 1], a[j]
             else:
                 break
+        print(f'{a=}')
     return a
 
 
-l = [8, 2, 6, 9, 1, 4, 3, 5]
-print(insert_sort(l))
+if __name__ == '__main__':
+    l = [4, 2, 1, 3]
+    print(l)
+    print('▼')
+    print(insert_sort(l))
