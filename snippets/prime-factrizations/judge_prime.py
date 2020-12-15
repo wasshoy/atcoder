b1fcfab497bd1df2190be87efaf435a5a141305c
A.Arimura <404ari@gmail.com>
2020-12-15 18:49:57 +0900

    ⚙️ スニペット修正
    素数判定のスニペットが間違っていたので修正

--- snippets/prime-factrizations/judge_prime.py
+++ snippets/prime-factrizations/judge_prime.py
@@ -5,6 +5,8 @@ import math
 def is_prime(n):
     if n == 1:
         return False
+    if n == 2:
+        return True
     if n % 2 == 0:
         return False
     for i in range(3, int(math.sqrt(n)) + 1, 2):
