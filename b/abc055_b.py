ef4f1784e783765539c98e01ff930d450ff9aac0
A.Arimura <404ari@gmail.com>
2021-01-13 15:51:52 +0900

    🐎  解答追加

---
+++ b/abc055_b.py
@@ -0,0 +1,23 @@
+import sys
+
+
+def input(): return sys.stdin.readline().strip()
+def input(): return sys.stdin.readline().strip()
+def I(): return int(input())
+def LI(): return list(map(int, input().split()))
+def IR(n): return [I() for i in range(n)]
+def LIR(n): return [LI() for i in range(n)]
+def SR(n): return [S() for i in range(n)]
+def S(): return input()
+def LS(): return input().split()
+
+
+INF = float('inf')
+MOD = 10**9 + 7
+
+n = I()
+ans = 1
+for i in range(1, n+1):
+    ans *= i
+    ans %= MOD
+print(ans)
